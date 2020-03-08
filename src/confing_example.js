47385bc6170cacaf8c3b87ec28d09971bd6ac073
gjpataky <gjpataky@yahoo.com>
2020-03-07 19:34:43 -0600

    initial

---
+++ src/confing_example.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "aaa79637952e4d16b082adccd4872f11";
+export const redirectUri = "http://localhost:3000/";
+export const scopes = [
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state"
+];
